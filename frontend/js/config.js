52db3f51abea8758df60241e568be21e2fd7d03c
Mayank Bhatt <bhattmayank350@gmail.com>
2026-06-28 07:25:55 +0530

    chore: Added config with safe values

---
+++ frontend/js/config.js
@@ -0,0 +1,4 @@
+// Public configuration – safe to commit
+window.SUPABASE_URL = 'https://emruaokypkqakoqiupxz.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_oH8P8ookzYBak6TujIJGuQ_ShS1-1s3';
+window.BACKEND_URL = 'http://localhost:3001/api';
